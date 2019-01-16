f065d83fa315c89c304c223897dbd05ef45098a6
Jefferson Zhan <30756446+jzhan2543@users.noreply.github.com>
2019-01-16 15:15:34 -0500

    update with timer

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ from unsplash.auth import Auth
 import urllib.request
 import os
 from appscript import app, mactypes
+import datetime
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-client_id = "77c38f1c232cbdde899e5163dce6572efa82d4275f6eb4614e5bf3a6a05c0843"
-client_secret = "3297d26247dfeb2ff322ddeef19b93c7ee0e3c55712a687915da7227ef1dab7d"
+
+client_id = {} #check API for own ids
+client_secret = {}
 redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
 code = ""
-
 auth = Auth(client_id, client_secret, redirect_uri, code=code)
 api = Api(auth)
-photo = api.photo.random(featured=True, orientation="landscape")
-url = photo[0].links.download
-urllib.request.urlretrieve(url, os.getcwd() + '/newimage.jpg')
-app('Finder').desktop_picture.set(mactypes.File(os.getcwd() + '/newimage.jpg'))
+
+sched = BlockingScheduler()
+
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=10) #This changes your background every weekday at 10AM as long as script is running
+@sched.scheduled_job('interval', seconds=10) #Changes your background every 10 seconds 
+def timed_job():
+    photo = api.photo.random(featured=True, orientation="landscape")
+    url = photo[0].links.download
+    urllib.request.urlretrieve(url, os.getcwd() + '/newimage' + str(datetime.datetime.date(datetime.datetime.now())) + '.jpg')
+    app('Finder').desktop_picture.set(mactypes.File(os.getcwd() + '/newimage' + str(datetime.datetime.date(datetime.datetime.now())) + '.jpg'))
+
+sched.start()
+
